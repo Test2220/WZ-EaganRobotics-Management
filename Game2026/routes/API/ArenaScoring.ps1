09b66984a92e84f0e459fe7de153c5719bece456
UrhoKarila <urhoebeling@gmail.com>
2026-01-31 13:37:21 -0600

    Update ArenaScoring.ps1
    Indentation change
    Pulled Mode out to a variable
    Added plan in comment

--- Game2026/routes/API/ArenaScoring.ps1
+++ Game2026/routes/API/ArenaScoring.ps1
@@ -1,33 +1,36 @@
 {
-            if ($webevent.method -eq "post") {
-                Lock-PodeObject -Name "points" -CheckGlobal -ScriptBlock {
-                    $pointState = Get-PodeState -Name "points" 
-                    if ($WebEvent.Parameters['team'] -match "red"){
-                        if ($WebEvent.Parameters['mode']-match "auto") {
-                            $pointState.RedAuto += $WebEvent.Parameters['score']
-                        }elseif($WebEvent.Parameters['mode']-match "tele") {
-                            $pointState.Redtele += $WebEvent.Parameters['score']
-                        }elseif($WebEvent.Parameters['mode']-match "end") {
-                            $pointState.redend += $WebEvent.Parameters['score']
-                        }
-                    }elseif ($WebEvent.Parameters['team'] -match "blue") {
-                        if ($WebEvent.Parameters['mode']-match "auto") {
-                            $pointState.BlueAuto += $WebEvent.Parameters['score']
-                        }elseif($WebEvent.Parameters['mode']-match "tele") {
-                            $pointState.BlueTele += $WebEvent.Parameters['score']
-                        }elseif($WebEvent.Parameters['mode']-match "end") {
-                            $pointState.BlueEnd += $WebEvent.Parameters['score']
-                        }
-                    }
-                    Set-PodeState -Name "points" -Value $pointState
-                    Write-PodeJsonResponse $pointState
+    #Mode should be fetched from the FMS - Auto, Teleop, or Endgame
+    $mode = $WebEvent.Parameters['mode']
+    # $mode = Invoke-WebRequest -Uri $CheesyUrl/state/I/Made/This/Up
+    if ($webevent.method -eq "post") {
+        Lock-PodeObject -Name "points" -CheckGlobal -ScriptBlock {
+            $pointState = Get-PodeState -Name "points" 
+            if ($WebEvent.Parameters['team'] -match "red"){
+                if ($mode -match "auto") {
+                    $pointState.RedAuto += $WebEvent.Parameters['score']
+                }elseif($mode -match "tele") {
+                    $pointState.Redtele += $WebEvent.Parameters['score']
+                }elseif($mode -match "end") {
+                    $pointState.redend += $WebEvent.Parameters['score']
                 }
-                
-            }else{
-                 Lock-PodeObject -Name "points" -CheckGlobal -ScriptBlock {
-                    $pointState = Get-PodeState -Name "points" 
-                    Write-PodeJsonResponse $pointState
-                } 
+            }elseif ($WebEvent.Parameters['team'] -match "blue") {
+                if ($mode -match "auto") {
+                    $pointState.BlueAuto += $WebEvent.Parameters['score']
+                }elseif($mode -match "tele") {
+                    $pointState.BlueTele += $WebEvent.Parameters['score']
+                }elseif($mode -match "end") {
+                    $pointState.BlueEnd += $WebEvent.Parameters['score']
+                }
             }
+            Set-PodeState -Name "points" -Value $pointState
+            Write-PodeJsonResponse $pointState
+        }
+        
+    }else{
+            Lock-PodeObject -Name "points" -CheckGlobal -ScriptBlock {
+            $pointState = Get-PodeState -Name "points" 
+            Write-PodeJsonResponse $pointState
+        } 
+    }
 
 }
