56aaf1cada5441cf038e4eb1d2048dd151a1461c
kashitakashi <kashi_takashi@twilightseed.com>
2026-02-09 14:02:05 -0600

    update server code for Config for Stacklight

--- server.ps1
+++ server.ps1
@@ -152,16 +152,10 @@ Start-PodeServer -Threads 4 -EnablePool WebSockets {
             Add-PodeRoute -Method Get -Path "/matchstart" -ScriptBlock {Send-PodeWebSocket -name "CA" -Message @{"type"="startMatch";"data"=@{"muteMatchSounds"=$false}}}
             Add-PodeRoute -Method Get -Path "/abortmatch" -ScriptBlock {Send-PodeWebSocket -name "CA" -Message @{"type"="abortMatch"}}
             Add-PodeRoute -Method Get -Path "/AudianceDisplay" -FilePath "./routes/API/API-AudianceDisplay.ps1"
-
-            Add-PodeRoute -Method Get -Path "/Arenastate" -ScriptBlock {
-                $message = Get-PodeState -Name "FMSArenaStatus"
-                Write-PodeJsonResponse -Value $message
+            Add-PodeRouteGroup -Path "/Stack"-Routes{
+               Add-PodeRoute -Path "/config" -Method Get,post -FilePath "./routes/API/Stacklight/config.ps1" 
+    
             }
-                        Add-PodeRoute -Method Get -Path "/Arenatimer" -ScriptBlock {
-                $message = Get-PodeState -Name "FMSArenatimer"
-                Write-PodeJsonResponse -Value $message
-            }
-            
             add-Poderoute -method get -path "/reset" -scriptblock {    Set-PodeState -Name 'points' -Value @{ 'RedAuto' = 0;'blueauto' = 0;'Redtele' = 0;'bluetele' = 0;'redend' = 0;'blueend' = 0;'redAutoL1' = 0;'redTeleL1' = 0;'redTeleL2' = 0;'redTeleL3' = 0;'BlueAutoL1' = 0;'BlueTeleL1' = 0;'BlueTeleL2' = 0;'BlueTeleL3' = 0; 'redMinorFoul' = 0;'redMajorFoul' = 0; 'blueMinorFoul'=0;'blueMajorFoul' = 0; } | Out-Null}
         }
         
