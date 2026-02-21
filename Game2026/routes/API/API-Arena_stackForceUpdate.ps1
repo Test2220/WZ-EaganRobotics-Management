57e2a26bb9a776948bcb287116ed24468ef1fede
kashitakashi <kashi_takashi@twilightseed.com>
2026-02-21 16:31:30 -0600

    bugfix

--- Game2026/routes/API/API-Arena_stackForceUpdate.ps1
+++ Game2026/routes/API/API-Arena_stackForceUpdate.ps1
@@ -207,7 +207,7 @@
         }# End 3rd Shift to  4th shift
         if (($time.MatchTimeSec -ge ($gametiming.endshift3)) -and ($time.MatchTimeSec -lT ($gametiming.endshift4warn))) {
             if ($shifttiming.shift3 -match "red"){#inverted due to unknonwn bug
-                if($StackStateMiddle.mappings.hub_red  -notmatch "on"){
+                if($StackStateMiddle.mappings.hub_red  -notmatch "off"){
                     Invoke-RestMethod -uri ("http://"+$StackConfigData.MiddleStack+":"+$StackConfigData.MiddleStackPort +"/set/7/off" ) -Method Post
                     #Code to disable red hubs for shift1
                     
@@ -243,7 +243,7 @@
                     #Code to enable Blue hubs for shift1
                     
                 }
-            }if ($shifttiming.shift3 -match "blue"){
+            }if ($shifttiming.shift3 -match "red"){
                 if($StackStateMiddle.mappings.hub_red  -notmatch "blink"){
                     Invoke-RestMethod -uri ("http://"+$StackConfigData.MiddleStack+":"+$StackConfigData.MiddleStackPort +"/set/7/blink" ) -Method Post
                     #Code to enable red hubs for shift1
